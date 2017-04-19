44a399530cfb5169ba9aa6ac54f7445a4d92bba2
Matt Oakes <hello@mattoakes.net>
2017-04-19 21:23:47 +0100

    Remove the develop command

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ var concat = require('metalsmith-concat')
 var layouts = require('metalsmith-layouts')
 var markdown = require('metalsmith-markdown')
 var permalinks = require('metalsmith-permalinks')
-var watch = require('metalsmith-watch')
-var metalsmithExpress = require('metalsmith-express')
 
 if (process.argv.length !== 3) {
-  console.error('Error: You must provide an action. One of "develop", "build" or "deploy".')
+  console.error('Error: You must provide an action. One of "build" or "deploy".')
   process.exit(-1)
 }
 
@@ -68,23 +66,6 @@ const website = Metalsmith(__dirname)
 
 // Perform the final actions depending on what action has been asked for
 switch (process.argv[2]) {
-  // Start a live reloading server which watches for changes
-  case 'develop':
-    website
-      .use(metalsmithExpress())
-      .use (
-        watch({
-          paths: {
-            '${source}/**/*': '${source}/**/*',
-            '${source}/../layouts/**/*': '${source}/**/*'
-          },
-          livereload: true
-        })
-      )
-      .build(function(err) {
-        if (err) throw err;
-      })
-    break
   // Build the website once and then exit
   case 'build':
     website
